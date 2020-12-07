00a08a7d375505d155370a85a6ae06c3f36118fd
Max Anselmo <itmanselmo@gmail.com>
2020-12-07 15:50:26 -0500

    Finished implementing a react frontend that recieves data from the hosted springboot server and hosts it on localhost:3000

--- cannabase-backend/src/main/java/com/example/cannabase/controller/UserController.java
+++ cannabase-backend/src/main/java/com/example/cannabase/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.cannabase.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.cannabase.model.User;
 import com.example.cannabase.repository.UserRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/")
 // Rest endpoint
